68eab2d8f7b51558de375e6f3fd8d674ba22c1a0
sebastian popescu <sebastianh123@yahoo.com>
2022-05-04 07:19:46 +0300

    Ex 2 Adaugare varianta in care folosesc while, numerele pot avea acum oricate cifre, nu doar 3.

--- Program.cs
+++ Program.cs
@@ -7,8 +7,8 @@ namespace exercitiiLaborator2
         static void Main(string[] args)
         {
             //Ex1();
-            //Ex2();
-            Ex3();
+            Ex2();
+            //Ex3();
         }
         static void Ex1()
         {
@@ -45,7 +45,117 @@ namespace exercitiiLaborator2
         {
             //Să se scrie un program care citeşte de la tastatură un şir de numere
             //naturale şi determină media aritmetică a celor pare.
-            Console.WriteLine("Introduceti un sir de numere de maxim 3 cifre, despartite de spatiu.");
+            //Console.WriteLine("Introduceti un sir de numere de maxim 3 cifre, despartite de spatiu.");
+            //string numere = (Console.ReadLine());
+            //string cifrePlusSpatiu = "01234 56789";
+            //for (int i = 0; i < numere.Length; i++)
+            //{
+            //    if (cifrePlusSpatiu.Contains(numere[i]))
+            //    {
+            //        continue;
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Introduceti doar numere pozitive! Nu introduceti litere sau alte caractere!");
+            //        return;
+            //    }
+            //}
+            //int numerotareCifre = 0;
+            //for (int i = 0; i < numere.Length; i++)
+            //{
+            //    if (numere[i]!=' ')
+            //    {
+            //        numerotareCifre++;
+            //    }
+            //    if (numerotareCifre>3)
+            //    {
+            //        Console.WriteLine("Introduceti numere de cel mult 3 cifre!");
+            //        return;
+            //    }
+            //    if (numere[i]==' ')
+            //    {
+            //        numerotareCifre = 0;
+            //    }
+            //}
+            //int suma = 0;
+            //int counter = 0;
+            //int medieAritmetica;
+            //for (int i = 0; i < numere.Length; i++)
+            //{
+            //    if (numere[i]==' ')
+            //    {
+            //        continue;
+            //    }
+            //    else if (i== numere.Length-1 && numere[i]!=' ')
+            //    {
+            //        int numarSir = int.Parse(numere[i].ToString());
+            //        if (numarSir == 0)
+            //        {
+            //            Console.WriteLine("Introduceti doar numere pozitive!");
+            //            return;
+            //        }
+            //        if (numarSir % 2 == 0)
+            //        {
+            //            suma += numarSir;
+            //            counter++;
+            //        }
+            //    }
+            //    else if (i < numere.Length - 1 && numere[i+1]==' ' || i + 1 >= numere.Length)
+            //    {
+            //        int numarSir = int.Parse(numere[i].ToString());
+            //        if (numarSir==0)
+            //        {
+            //            Console.WriteLine("Introduceti doar numere pozitive!");
+            //            return;
+            //        }
+            //        if (numarSir%2==0)
+            //        {
+            //            suma += numarSir;
+            //            counter++;
+            //        }
+            //    }
+            //    else if (i < numere.Length - 2 && numere[i+2]==' '|| i+2>= numere.Length)
+            //    {
+            //        string sirTemporar = numere[i].ToString() + numere[i + 1].ToString();
+            //        int numarSir = int.Parse(sirTemporar);
+            //        if (numarSir % 2 == 0)
+            //        {
+            //            suma += numarSir;
+            //            counter++;
+            //            i = i + 1;
+            //        }
+            //        else
+            //        {
+            //            i = i + 1;
+            //        }
+            //    }
+            //    else if (i < numere.Length - 2 && numere[i+2]!=' ' || i + 2 >= numere.Length)
+            //    {
+            //        string sirTemporar = numere[i].ToString() + numere[i + 1].ToString()+ numere[i+2].ToString();
+            //        int numarSir = int.Parse(sirTemporar);
+            //        if (numarSir % 2 == 0)
+            //        {
+            //            suma += numarSir;
+            //            counter++;
+            //            i = i + 2;
+            //        }
+            //        else
+            //        {
+            //            i = i + 2;
+            //        }
+            //    }
+            //}
+            //if (counter<2)
+            //{
+            //    Console.WriteLine("Nu au fost introduse cel putin 2 numere pare.");
+            //    return;
+            //}
+            //medieAritmetica = suma / counter;
+            //Console.WriteLine("Media aritmetica a numerelor pare introduse este " + medieAritmetica);
+
+            //Varianta a 2a cu while
+
+            Console.WriteLine("Introduceti un sir de numere despartite de spatiu.");
             string numere = (Console.ReadLine());
             string cifrePlusSpatiu = "01234 56789";
             for (int i = 0; i < numere.Length; i++)
@@ -60,97 +170,42 @@ namespace exercitiiLaborator2
                     return;
                 }
             }
-            int numerotareCifre = 0;
-            for (int i = 0; i < numere.Length; i++)
-            {
-                if (numere[i]!=' ')
-                {
-                    numerotareCifre++;
-                }
-                if (numerotareCifre>3)
-                {
-                    Console.WriteLine("Introduceti numere de cel mult 3 cifre!");
-                    return;
-                }
-                if (numere[i]==' ')
-                {
-                    numerotareCifre = 0;
-                }
-            }
             int suma = 0;
-            int counter = 0;
+            int numerotareNumerePare = 0;
             int medieAritmetica;
+            string sirTemporar = "";
             for (int i = 0; i < numere.Length; i++)
             {
-                if (numere[i]==' ')
+                sirTemporar = "";
+                int startI = i;
+                while (numere[i] != ' ')
                 {
-                    continue;
-                }
-                else if (i== numere.Length-1 && numere[i]!=' ')
-                {
-                    int numarSir = int.Parse(numere[i].ToString());
-                    if (numarSir == 0)
+                    i++;
+                    if (i == numere.Length)
                     {
-                        Console.WriteLine("Introduceti doar numere pozitive!");
-                        return;
-                    }
-                    if (numarSir % 2 == 0)
-                    {
-                        suma += numarSir;
-                        counter++;
+                        break;
                     }
                 }
-                else if (i < numere.Length - 1 && numere[i+1]==' ' || i + 1 >= numere.Length)
+                for (int j = startI; j < i; j++)
                 {
-                    int numarSir = int.Parse(numere[i].ToString());
-                    if (numarSir==0)
+                    sirTemporar = sirTemporar + numere[j].ToString();
+                    if (j == i - 1)
                     {
-                        Console.WriteLine("Introduceti doar numere pozitive!");
-                        return;
-                    }
-                    if (numarSir%2==0)
-                    {
-                        suma += numarSir;
-                        counter++;
-                    }
-                }
-                else if (i < numere.Length - 2 && numere[i+2]==' '|| i+2>= numere.Length)
-                {
-                    string sirTemporar = numere[i].ToString() + numere[i + 1].ToString();
-                    int numarSir = int.Parse(sirTemporar);
-                    if (numarSir % 2 == 0)
-                    {
-                        suma += numarSir;
-                        counter++;
-                        i = i + 1;
-                    }
-                    else
-                    {
-                        i = i + 1;
-                    }
-                }
-                else if (i < numere.Length - 2 && numere[i+2]!=' ' || i + 2 >= numere.Length)
-                {
-                    string sirTemporar = numere[i].ToString() + numere[i + 1].ToString()+ numere[i+2].ToString();
-                    int numarSir = int.Parse(sirTemporar);
-                    if (numarSir % 2 == 0)
-                    {
-                        suma += numarSir;
-                        counter++;
-                        i = i + 2;
-                    }
-                    else
-                    {
-                        i = i + 2;
+                        int numarSir = int.Parse(sirTemporar);
+                        if (numarSir % 2 == 0)
+                        {
+                            suma += numarSir;
+                            numerotareNumerePare++;
+                        }
                     }
                 }
             }
-            if (counter<2)
+            if (numerotareNumerePare < 2)
             {
                 Console.WriteLine("Nu au fost introduse cel putin 2 numere pare.");
                 return;
             }
-            medieAritmetica = suma / counter;
+            medieAritmetica = suma / numerotareNumerePare;
             Console.WriteLine("Media aritmetica a numerelor pare introduse este " + medieAritmetica);
         }
         static void Ex3()
